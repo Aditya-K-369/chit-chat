93d76cbc885f620df5e10bb3502e7e9e4cf97957
Aditya-K-369 <aditya369ajay@gmail.com>
2024-04-02 18:32:43 +0530

    new logic

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
+let img=document.querySelector(".img img");
 let msgPara = document.querySelector(".msg-para");
-let buttons = document.querySelectorAll(".button");
-let img = document.querySelector(".img img ");
-msgPara.innerText ="";
-const clicked = ()=>{
-buttons.forEach((button)=>{
-    button.addEventListener("click",()=>{
-        const value = button.value;
-        check(value);
-    });
+let buttonNo=document.querySelector(".button-no");
+let buttonYes = document.querySelector(".button-yes");
+
+buttonNo.addEventListener("mouseover",(e)=>{
+console.log("hi");
 });
-};
-clicked();
-const check =(value)=>{
-if(value==="no"){
-    console.log(value);
-    msgPara.innerText="Really?"
-   let newsrc = `https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F914.png`;
-   img.src = newsrc;
-}
-}
